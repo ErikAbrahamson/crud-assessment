8838de9b1b37203e6ed5e593430d57d432f4382f
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-09 12:34:47 -0600

    install mongolab uri

--- server/database.js
+++ server/database.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
 var Schema = mongoose.Schema;
+var URI = uriUtil.formatMongoose('mongodb://heroku_mgr200d3:ioc6l977u77vvau91bo7emghae@ds051848.mongolab.com:51848/heroku_mgr200d3');
 
 var dog = new Schema({
   name: String,
@@ -7,5 +9,20 @@ var dog = new Schema({
   breed: String
 });
 
+var options = {
+  server: {
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS: 30000
+    }
+  },
+  replset: {
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS : 30000
+    }
+  }
+};
+
 mongoose.model('dogs', dog);
-mongoose.connect(null || 'mongodb://localhost/crud-assessment');
+mongoose.connect(URI, options);
