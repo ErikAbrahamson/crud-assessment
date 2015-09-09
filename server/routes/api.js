8753acf1e912db5ff5ea2f4850f67743f4017480
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-09 11:07:07 -0600

    add get method

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,17 +3,25 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Dog = mongoose.model('dogs');
 
+// SPA Render
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CRUD Assessment' });
 });
 
+// API Form Post
 router.post('/dogs', function(req, res, next) {
   new Dog(req.body).save(function(err, success) {
-    console.log(success);
-    res.json({
-      message: 'Dog added successfully'
-    });
+    console.log(req.body);
+    res.json(success);
   });
 });
+
+// API Get Collection
+router.get('/dogs', function(req, res, next) {
+  Dog.find(function(err, dogs) {
+    res.json(dogs);
+  });
+});
+
 
 module.exports = router;
